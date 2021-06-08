1243d9c1841aa7663c4e86f5d3522cc181448dbb
Maciej Drozdowicz <maciek.d@me.com>
2021-06-08 11:18:44 +0200

    Initial commit

---
+++ AppConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace BlobManager
+{
+    public class AppConfig
+    {
+        public string BlobServiceEndpoint { get; set; }
+        public string StorageAccountName { get; set; }
+        public string StorageAccountKey { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,96 @@
+﻿using Azure.Storage;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using BlobManager;
+using Microsoft.Extensions.Configuration;
+
+public class Program
+{
+    private static string blobServiceEndpoint = "";
+    private static string storageAccountName = "";
+    private static string storageAccountKey = "";
+
+    public static async Task Main(string[] args)
+    {
+        //var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false);
+
+        IConfiguration config = builder.Build();
+
+        var appConfig = config.GetSection("AppConfig").Get<AppConfig>();
+
+        blobServiceEndpoint = appConfig.BlobServiceEndpoint;
+        storageAccountName = appConfig.StorageAccountName;
+        storageAccountKey = appConfig.StorageAccountKey;
+
+
+
+        StorageSharedKeyCredential accountCredentials = new StorageSharedKeyCredential(storageAccountName, storageAccountKey);
+
+        BlobServiceClient serviceClient = new BlobServiceClient(new Uri(blobServiceEndpoint), accountCredentials);
+
+        AccountInfo info = await serviceClient.GetAccountInfoAsync();
+        
+
+        await Console.Out.WriteLineAsync($"Connected to Azure Storage Account");
+        await Console.Out.WriteLineAsync($"Account name:\t{storageAccountName}");
+        await Console.Out.WriteLineAsync($"Account kind:\t{info?.AccountKind}");
+        await Console.Out.WriteLineAsync($"Account sku:\t{info?.SkuName}");
+
+        await EnumerateContainersAsync(serviceClient);
+
+        string existingContainerName = "raster-graphics";
+        await EnumerateBlobsAsync(serviceClient, existingContainerName);
+
+        string newContainerName = "vector-graphics";
+        BlobContainerClient containerClient = await GetContainerAsync(serviceClient, newContainerName);
+
+        string uploadedBlobName = "graph.svg";
+        BlobClient blobClient = await GetBlobAsync(containerClient, uploadedBlobName);
+
+        await Console.Out.WriteLineAsync($"Blob Url:\t{blobClient.Uri}");
+    }
+
+    private static async Task EnumerateContainersAsync(BlobServiceClient client)
+    {
+        await foreach (BlobContainerItem container in client.GetBlobContainersAsync())
+        {
+            await Console.Out.WriteLineAsync($"Container:\t{container.Name}");
+        }
+    }
+
+    private static async Task EnumerateBlobsAsync(BlobServiceClient client, string containerName)
+    {
+        BlobContainerClient container = client.GetBlobContainerClient(containerName);
+
+        await Console.Out.WriteLineAsync($"Searching:\t{container.Name}");
+
+        await foreach (BlobItem blob in container.GetBlobsAsync())
+        {
+            await Console.Out.WriteLineAsync($"Existing Blob:\t{blob.Name}");
+        }
+    }
+
+    private static async Task<BlobContainerClient> GetContainerAsync(BlobServiceClient client, string containerName)
+    {
+        BlobContainerClient container = client.GetBlobContainerClient(containerName);
+
+        await container.CreateIfNotExistsAsync(PublicAccessType.Blob);
+
+        await Console.Out.WriteLineAsync($"New Container:\t{container.Name}");
+
+        return container;
+    }
+
+    private static async Task<BlobClient> GetBlobAsync(BlobContainerClient client, string blobName)
+    {
+        BlobClient blob = client.GetBlobClient(blobName);
+        await Console.Out.WriteLineAsync($"Blob Found:\t{blob.Name}");
+        return blob;
+    }
+}
